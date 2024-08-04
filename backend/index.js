f315411dde0371f5ea8b38f41761279956792939
shalam <73836150+shaaalaaam@users.noreply.github.com>
2024-08-04 16:26:45 +0530

    backend updated to work with MongoDB Atlas

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,5 @@
+require('dotenv').config(); 
+
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
@@ -25,10 +27,19 @@ app.post('/api/questions', async (req, res) => {
   }
 });
 
+//Can shift these thigns to .env file. ( Q: Can there be more than 1 env file?)
+
+
+const { DB_USERNAME, DB_PASSWORD } = process.env;
+// console.log(`DB_USERNAME : ${DB_USERNAME}`);
+// console.log(`DB_PASSWORD : ${DB_PASSWORD}`);
+
+const MONGODB_URI = `mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@form-declutter-cluster.hmkf843.mongodb.net/qa_db?retryWrites=true&w=majority`;
+
 // Connect to MongoDB
-mongoose.connect('mongodb://localhost:27017/yourDatabase', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.error('MongoDB connection error:', err));
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
